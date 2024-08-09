e6ae79efa39190a1bc934480ea5c7ae82a0bdaae
Ahmet Arda Kavakci <ahmetardakavakci@gmail.com>
2024-08-09 18:36:22 +0300

    Create slice type practice

---
+++ 09_slice_type/src/main.rs
@@ -0,0 +1,23 @@
+fn main() {
+    let s = String::from("Merhaba dünya!");
+    let word = first_word(&s);
+    println!("{}", word);
+
+    let arr = [1, 2, 3, 4, 5, 6];
+    let slice: &[i8] = &arr[3..5];
+    let slice2: &[i32] = &arr[0..3];
+
+    println!("{:?}", slice)
+}
+
+fn first_word(s: &String) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+
+    &s[..]
+}

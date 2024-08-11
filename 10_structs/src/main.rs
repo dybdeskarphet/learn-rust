b006880183e69531b4e8aac690a0f2e8a309d190
Ahmet Arda Kavakci <ahmetardakavakci@gmail.com>
2024-08-11 11:44:22 +0300

    Practice structs

--- 10_structs/src/main.rs
+++ 10_structs/src/main.rs
@@ -1,4 +1,11 @@
 fn main() {
+    struct User {
+        active: bool,
+        username: String,
+        email: String,
+        sign_in_count: u32,
+    };
+
     struct Book {
         name: String,
         release_date: u16,
@@ -16,13 +23,29 @@ fn main() {
     };
 
     println!("{:?}", uvercinka.name);
-}
 
-fn build_user(email: String, username: String) -> User {
-    User {
-        active: true,
-        username,
-        email,
+    fn build_user(email: String, username: String) -> User {
+        User {
+            active: true,
+            username,
+            email,
+            sign_in_count: 1,
+        }
+    };
+
+    let user1 = User {
+        email: String::from("deneme123@gmail.com"),
+        username: String::from("onionland"),
         sign_in_count: 1,
-    }
+        active: true,
+    };
+
+    let user2 = User {
+        username: String::from("garlicland"),
+        email: String::from("deneme1234@gmail.com"),
+        ..user1
+    };
+
+    println!("{:?}", user1.sign_in_count);
+    println!("{:?}", user2.sign_in_count);
 }

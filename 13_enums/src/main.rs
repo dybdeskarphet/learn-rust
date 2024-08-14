ea47947cc403a1c1a098839da2a6e175975bcf13
Ahmet Arda Kavakci <ahmetardakavakci@gmail.com>
2024-08-14 12:28:58 +0300

    Add one more example to enums

--- 13_enums/src/main.rs
+++ 13_enums/src/main.rs
@@ -1,22 +1,50 @@
-/*
-#[derive(Debug)]
-enum IpAddrKind {
-    V4,
-    V6,
-}
-
-struct IpAddr {
-    kind: IpAddrKind,
-    address: String,
-} */
-
-#[derive(Debug)]
-enum IpAddr {
-    V4(u8, u8, u8, u8),
-    V6(String),
-}
+use std::borrow::Borrow;
 
 fn main() {
+    // ipaddr_enum()
+    message_enum();
+}
+
+fn message_enum() {
+    enum Message {
+        Quit,
+        Move { x: i32, y: i32 },
+        Write(String),
+        ChangeColor(i32, i32, i32),
+    }
+
+    impl Message {
+        fn call(&self) {
+            match self {
+                Message::Write(text) => println!("{}", text),
+                _ => (),
+            }
+        }
+    }
+
+    let msg = Message::Write(String::from("Merhaba Rust!"));
+    msg.call();
+}
+
+fn ipaddr_enum() {
+    /*
+    #[derive(Debug)]
+    enum IpAddrKind {
+        V4,
+        V6,
+    }
+
+    struct IpAddr {
+        kind: IpAddrKind,
+        address: String,
+    } */
+
+    #[derive(Debug)]
+    enum IpAddr {
+        V4(u8, u8, u8, u8),
+        V6(String),
+    }
+
     let home = IpAddr::V4(127, 0, 0, 1);
     let loopback = IpAddr::V6(String::from("::1"));
     /* let home = IpAddr {

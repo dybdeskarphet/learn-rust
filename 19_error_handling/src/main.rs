a013e370435984af3d2d75cf5593936ebd414e08
Ahmet Arda Kavakci <ahmetardakavakci@gmail.com>
2024-08-22 16:45:07 +0300

    Finish error handling practice

--- 19_error_handling/src/main.rs
+++ 19_error_handling/src/main.rs
@@ -1,16 +1,22 @@
 use std::fs::File;
-use std::io::ErrorKind;
+use std::error::Error;
 
-fn main() {
-    //    verbose_error_handling();
-    simple_error_handling();
+fn main() -> Result<(), Box<dyn Error>> {
+    // verbose_error_handling();
+    // simple_error_handling();
+    // read_username_from_file();
+   let greeting_file = File::open("hello.txt")?;
+   Ok(())
 }
 
+/*
 fn simple_error_handling() {
     let _greeting_file =
         File::open("hello.txt").expect("hello.txt should be included in this project");
 }
+*/
 
+/*
 fn verbose_error_handling() {
     let greeting_file_result = File::open("hello.txt");
 
@@ -27,5 +33,17 @@ fn verbose_error_handling() {
         },
     };
 }
+
+fn read_username_from_file() -> Result<String, io::Error> {
+    fs::read_to_string("hello.txt")
+}
+
+// `?` can be used for Option results too. In this case, if there is no char, it will return None.
+fn last_char_of_first_line(text: &str) -> Option<char> {
+    // We used `?` after next becuase next gets the first line
+    text.lines().next()?.chars().last()
+}
+
+*/
 
 // You're here: https://doc.rust-lang.org/stable/book/ch09-02-recoverable-errors-with-result.html#propagating-errors

9fcbc9ff4f9d5458fc174e9aa4ec7d097f915dfc
Ahmet Arda Kavakci <ahmetardakavakci@gmail.com>
2024-08-28 03:10:08 +0300

    Add enum practice

---
+++ rust_by_example/05_enums/src/main.rs
@@ -0,0 +1,25 @@
+enum Calculator {
+    Add,
+    Substract,
+    Mulitply,
+    Divide
+}
+
+impl Calculator {
+    fn run(&self, x: i32, y: i32) -> i32 {
+        match self {
+            Self::Add => x + y,
+            Self::Substract => x - y,
+            Self::Divide => x / y,
+            Self::Mulitply => x * y,
+        }
+    }
+}
+
+fn main() {
+    use crate::Calculator::{Add,Substract,Divide,Mulitply};
+
+    let ops = (Add.run(10,40), Substract.run(40,10), Divide.run(63,3), Mulitply.run(33,3));
+
+    println!("{:?}", ops);
+}

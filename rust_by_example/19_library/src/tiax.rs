94e3df9c3b4139aeb753193683ab7a34345d8f13
Ahmet Arda Kavakci <ahmetardakavakci@gmail.com>
2024-08-31 19:59:15 +0300

    Add library attributes to tiax

--- rust_by_example/19_library/src/tiax.rs
+++ rust_by_example/19_library/src/tiax.rs
@@ -1,3 +1,9 @@
+// NOTE: With the above attributes, we can use rustc directly
+// and it will detect this file as a library.
+
+#![crate_type = "lib"]
+#![crate_name = "tiax"]
+
 pub fn public_function() {
     println!("This is a public function.");
 }
